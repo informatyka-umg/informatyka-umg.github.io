02e97bf1e600efd57bd738b442d7e382933a59f0
Artur Zacniewski <a.zacniewski@gmail.com>
2025-06-24 10:06:21 +0200

    Add files after backup

--- plany/plan-informatyka-umg.js
+++ plany/plan-informatyka-umg.js
@@ -1,6 +1,30 @@
-let data = {
+const data = {
     "typ": "inżynierskie",
-    "semestr": 1
+    "semestry": {
+        1: {
+           "I. Przedmioty ogólne i kierunkowe": {
+               "1. Wychowanie fizyczne": {
+                   "Rygor": "zaliczenie",
+                   "Liczba godzin": 15,
+                   "Punkty ECTS": 0,
+                   "Prowadzący": "SWFiS",
+                   "Opis": "Np. Podstawowe algorytmy przetwarzania obrazów, histogramy",
+                   "Narzędzia, języki, technologie": "Np. C++, OpenCV, system kontroli wersji git"
+               },
+               "2. Język angielski": {
+
+               }
+
+           },
+            " II. Przedmioty wybieralne": {
+
+            }
+        },
+
+        2: {
+
+        }
+    }
     };
 
 export {data};
